07173ef9009a1e27e767a2d1dff35f858fb4fea3
ismaelp96 <ismael6991@live.com>
2025-04-16 11:37:48 -0300

    feat: page login, register, routes and styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,23 +5,35 @@ const Navbar = () => {
 	const isActive = ({ isActive }) => (isActive ? styles.active : '');
 
 	return (
-		<nav className={styles.navbar}>
-			<NavLink to='/' className={styles.brand}>
-				Mini <span>Blog</span>
-			</NavLink>
-			<ul className={styles.links_list}>
-				<li>
-					<NavLink to='/' className={isActive}>
-						Home
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/about' className={isActive}>
-						Sobre
-					</NavLink>
-				</li>
-			</ul>
-		</nav>
+		<header className={styles.header}>
+			<nav className={styles.navbar}>
+				<NavLink to='/' className={styles.brand}>
+					Mini <span>Blog</span>
+				</NavLink>
+				<ul className={styles.links_list}>
+					<li>
+						<NavLink to='/' className={isActive}>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/about' className={isActive}>
+							Sobre
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/login' className={isActive}>
+							Entrar
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/register' className={isActive}>
+							Cadastrar
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 };
 
