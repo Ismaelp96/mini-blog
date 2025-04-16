58495d457a84c957160189f2507f8436b33706b0
ismaelp96 <ismael6991@live.com>
2025-04-15 21:48:44 -0300

    feat: components nav and footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<BrowserRouter>
 				<div className='container'>
+					<Navbar />
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/about' element={<About />} />
 					</Routes>
+					<Footer />
 				</div>
 			</BrowserRouter>
 		</div>
