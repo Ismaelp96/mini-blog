2243cef42064977fb25552ffa76a53b44d0a870f
ismaelp96 <ismael6991@live.com>
2025-04-23 09:53:46 -0300

    feat: Estrutura dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,34 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Dashboard.module.css';
+
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 const Dashboard = () => {
-	return <h1>Dashboard</h1>;
+	const { user } = useAuthValue();
+	const uid = user.uid;
+	const posts = [];
+	return (
+		<div>
+			<h2>Dashboard</h2>
+			<p>Seja bem-vindo, {user.displayName}</p>
+			<p>Seu email: {user.email}</p>
+
+			<p>Gerencie os seus posts</p>
+			{posts && posts.length === 0 ? (
+				<div className={styles.nopost}>
+					<p>Não foram encontrados posts...</p>
+					<Link to='/posts/create' className='btn btn-primary'>
+						Criar post
+					</Link>
+				</div>
+			) : (
+				<div>
+					<p>tem posts!</p>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Dashboard;
